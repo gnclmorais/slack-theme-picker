06b2159bcc0264912b138c0b5d978f0d41f1285e
Gonçalo Morais <gnclmorais@gmail.com>
2016-09-24 15:34:33 +0100

    Bring vue-color to try to get colour pickers.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,44 @@
-new Vue({
-  el: '.sidebar',
+var defaultProps = {
+  hex: '#194d33',
+  hsl: {
+    h: 150,
+    s: 0.5,
+    l: 0.2,
+    a: 1,
+  },
+  hsv: {
+    h: 150,
+    s: 0.66,
+    v: 0.30,
+    a: 1,
+  },
+  rgba: {
+    r: 25,
+    g: 77,
+    b: 51,
+    a: 1,
+  },
+  a: 1,
+};
 
-  data: {
-    slackColumnBg:       'salmon',
-    slackMenuBgHover:    'tomato',
-    slackActiveItem:     '#ffffff',
-    slackActiveItemText: '#ffffff',
-    slackHoverItem:      'red',
-    slackTextColor:      '#000000',
-    slackActivePresence: 'green',
-    slackMentionBadge:   'rebeccapurple',
+new Vue({
+  el: '.app',
+
+  data: function () {
+    return {
+      slackColumnBg:       'salmon',
+      slackMenuBgHover:    'tomato',
+      slackActiveItem:     'white',
+      slackActiveItemText: 'black',
+      slackHoverItem:      'red',
+      slackTextColor:      'black',
+      slackActivePresence: 'green',
+      slackMentionBadge:   'rebeccapurple',
+      colors: defaultProps,
+    };
+  },
+
+  components: {
+    'chrome-picker': VueColor.Chrome,
   },
 });
